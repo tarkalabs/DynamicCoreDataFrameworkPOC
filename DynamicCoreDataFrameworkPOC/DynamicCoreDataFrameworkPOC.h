3f046c42d15998fe7bfdd6fea691757a7cec415a
Prasanna <prasanna@tarkalabs.com>
2016-05-02 18:06:16 +0530

    Initial Commit

---
+++ DynamicCoreDataFrameworkPOC/DynamicCoreDataFrameworkPOC.h
@@ -0,0 +1,19 @@
+//
+//  DynamicCoreDataFrameworkPOC.h
+//  DynamicCoreDataFrameworkPOC
+//
+//  Created by Prasanna on 02/05/16.
+//  Copyright © 2016 Tarka Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DynamicCoreDataFrameworkPOC.
+FOUNDATION_EXPORT double DynamicCoreDataFrameworkPOCVersionNumber;
+
+//! Project version string for DynamicCoreDataFrameworkPOC.
+FOUNDATION_EXPORT const unsigned char DynamicCoreDataFrameworkPOCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicCoreDataFrameworkPOC/PublicHeader.h>
+
+
